d9e37447037c0125c29b62f97b8d8208e931afa5
rodrigolustosa <rodrigo.lustosa@usp.br>
2022-06-04 12:45:11 -0300

    write complete table environment

--- README.md
+++ README.md
@@ -3,14 +3,43 @@ Some R functions (one for now) I made to better integrate R and LaTeX.
 
 ## Functions
 ### write_latexTable
-`write_latexTable` writes a data frame as a file in the same format as a tabular in LaTeX. You can open the file and just copy and paste it in LaTeX. Example:
+
+`write_latexTable` writes a data frame as a file in the same format as a tabular inside a table in LaTeX. You can open the file and just copy and paste it in LaTeX. Example:
 ```
-write_latexTable(mtcars[1:3,1:5],"test.txt")
+write_latexTable(mtcars[1:3,1:5], "test.txt")
 ```
 Inside `text.txt` you will find:
 ```
-mpg & cyl & disp & hp & drat \\
-21 & 6 & 160 & 110 & 3.9 \\
-21 & 6 & 160 & 110 & 3.9 \\
-22.8 & 4 & 108 & 93 & 3.85 \\
+	\begin{table}[htbp]
+		\centering
+		\caption{}
+		\begin{tabular}{ccccc}
+			mpg & cyl & disp & hp & drat \\
+			21 & 6 & 160 & 110 & 3.9 \\
+			21 & 6 & 160 & 110 & 3.9 \\
+			22.8 & 4 & 108 & 93 & 3.85 \\
+		\end{tabular}
+		\label{tab:}
+	\end{table}
 ```
+
+You can pass more information to `write_latexTable` like caption, table position in text, columns alignment and table label. For example:
+```
+write_latexTable(mtcars[1:3,1:5], caption="Some information", pos="h", align="r|cccc", label="tab:example", "test.txt")
+```
+Inside `text.txt` now you will find:
+```
+	\begin{table}[h]
+		\centering
+		\caption{Some information}
+		\begin{tabular}{r|cccc}
+			mpg & cyl & disp & hp & drat \\
+			21 & 6 & 160 & 110 & 3.9 \\
+			21 & 6 & 160 & 110 & 3.9 \\
+			22.8 & 4 & 108 & 93 & 3.85 \\
+		\end{tabular}
+		\label{tab:example}
+	\end{table}
+```
+
+
--- R_functions_4_latex.R
+++ R_functions_4_latex.R
@@ -9,34 +9,110 @@
 # write a data frame as a file in the same format as a tabular in LaTeX
 # you can open the file and just copy and paste in LaTeX
 # R packages used: stringr
-write_latexTable <- function(x,file,siunitx_package = FALSE){
+write_latexTable <- function(x,file,caption="",pos="htbp",align="c",label="tab:",
+                             centering = TRUE, siunitx_package = FALSE){
+  # inner functions
   make_latexTable_line <- function(raw_line){
     final_line <- paste(raw_line,collapse = " & ")
     final_line <- paste(final_line,"\\\\")
+    final_line <- paste("\t\t\t",final_line,sep = "")
     final_line <- stringr::str_replace_all(final_line,"_"," ")
     return(final_line)
   }
-  # coersing a possible tibble to a dataframe
-  x <- data.frame(x)
-  # extracting basic info
-  n <- nrow(x)
-  n_col <- ncol(x)
-  header <- names(x)
-  # add commands from siunitx package
-  if(siunitx_package)
-    for(j in 1:ncol(x))
-      if(class(x[,j]) == "numeric")
-        # \num{} is a command from siunitx package which 
-        # makes numbers format more pleasant
-        x[,j] <- paste("\\num{",x[,j],"}",sep="")
-  # write file
-  con <- file(file,"w")
-  header_save <- make_latexTable_line(header)
-  writeLines(header_save,con)
-  for(i in 1:n){
-    line <- x[i,]
-    line_save <- make_latexTable_line(line)
-    writeLines(line_save,con)
+  wrt_tbl_env_start <- function(con,pos,caption,centering){
+    # base
+    tbl_string_begin <- "\\begin{table}"
+    tbl_string_centering <- "\\centering"
+    tbl_string_caption <- "\\caption"
+    # add info given by user
+    tbl_string_begin   <- paste(tbl_string_begin,"[",pos,"]",sep = "")
+    tbl_string_caption <- paste(tbl_string_caption,"{",caption,"}",sep = "")
+    # add tabs
+    tbl_string_begin     <- paste("\t",tbl_string_begin,sep = "")
+    tbl_string_centering <- paste("\t\t",tbl_string_centering,sep = "")
+    tbl_string_caption   <- paste("\t\t",tbl_string_caption,sep = "")
+    # write lines
+    writeLines(tbl_string_begin,con)
+    if(centering)
+      writeLines(tbl_string_centering,con)
+    writeLines(tbl_string_caption,con)
   }
+  wrt_tblr_env_start <- function(con,n_col,align){
+    # base
+    tblr_string_begin <- "\\begin{tabular}"
+    # correct number of columns
+    n_given <- str_count(align)
+    if(n_given == 1)
+      align <- paste(rep(align,n_col),collapse = "")
+    # add info given by user
+    tblr_string_begin <- paste(tblr_string_begin,"{",align,"}",sep = "")
+    # add tabs
+    tblr_string_begin <- paste("\t\t",tblr_string_begin,sep = "")
+    # write lines
+    writeLines(tblr_string_begin,con)
+  }
+  wrt_tbl_env_end <- function(con,label){
+    # base
+    tbl_string_label <- "\\label"
+    tbl_string_end <- "\\end{table}"
+    # add info given by user
+    tbl_string_label <- paste(tbl_string_label,"{",label,"}",sep = "")
+    # add tabs
+    tbl_string_label <- paste("\t\t",tbl_string_label,sep = "")
+    tbl_string_end   <- paste("\t",tbl_string_end,sep = "")
+    # write lines
+    writeLines(tbl_string_label,con)
+    writeLines(tbl_string_end,con)
+  }
+  wrt_tblr_env_end <- function(con){
+    # base
+    tblr_string_end <- "\\end{tabular}"
+    # add tabs
+    tblr_string_end <- paste("\t\t",tblr_string_end,sep = "")
+    # write lines
+    writeLines(tblr_string_end,con)
+  }
+  wrt_tabular <- function(con,x,align,siunitx_package){
+    # coersing a possible tibble to a dataframe
+    x <- data.frame(x)
+    
+    # extracting basic info
+    n <- nrow(x)
+    n_col <- ncol(x)
+    header <- names(x)
+    
+    # start tabular environment
+    wrt_tblr_env_start(con,n_col,align)
+    
+    # add commands from siunitx package
+    if(siunitx_package)
+      for(j in 1:ncol(x))
+        if(class(x[,j]) == "numeric")
+          # \num{} is a command from siunitx package which 
+          # makes numbers format more pleasant
+          x[,j] <- paste("\\num{",x[,j],"}",sep="")
+    
+    # write header and lines
+    header_save <- make_latexTable_line(header)
+    writeLines(header_save,con)
+    for(i in 1:n){
+      line <- x[i,]
+      line_save <- make_latexTable_line(line)
+      writeLines(line_save,con)
+    }
+    
+    # end tabular environment
+    wrt_tblr_env_end(con)
+  }
+  
+  # open file
+  con <- file(file,"w")
+  # start table environment
+  wrt_tbl_env_start(con,pos,caption,centering)
+  # write tabular
+  wrt_tabular(con,x,align,siunitx_package)
+  # end table environment
+  wrt_tbl_env_end(con,label)
+  # close file
   close(con)
 }
