4fd3981e41d910aa6ba473e0c4e6a41c7a455a71
rodrigolustosa <rodrigo.lustosa@usp.br>
2022-06-04 11:26:07 -0300

    organize steps to make latex tabular line

--- R_functions_4_latex.R
+++ R_functions_4_latex.R
@@ -10,6 +10,12 @@
 # you can open the file and just copy and paste in LaTeX
 # R packages used: stringr
 write_latexTable <- function(x,file,siunitx_package = FALSE){
+  make_latexTable_line <- function(raw_line){
+    final_line <- paste(raw_line,collapse = " & ")
+    final_line <- paste(final_line,"\\\\")
+    final_line <- stringr::str_replace_all(final_line,"_"," ")
+    return(final_line)
+  }
   # coersing a possible tibble to a dataframe
   x <- data.frame(x)
   # extracting basic info
@@ -25,10 +31,12 @@ write_latexTable <- function(x,file,siunitx_package = FALSE){
         x[,j] <- paste("\\num{",x[,j],"}",sep="")
   # write file
   con <- file(file,"w")
-  writeLines(stringr::str_replace_all(paste(paste(header,collapse = " & "),"\\\\"),"_"," "),con)
+  header_save <- make_latexTable_line(header)
+  writeLines(header_save,con)
   for(i in 1:n){
     line <- x[i,]
-    writeLines(stringr::str_replace_all(paste(paste(line,collapse = " & "),"\\\\"),"_"," "),con)
+    line_save <- make_latexTable_line(line)
+    writeLines(line_save,con)
   }
   close(con)
 }
