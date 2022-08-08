4bf109794444e108b92161f165370a6217567e54
rodrigolustosa <rodrigo.lustosa@usp.br>
2022-08-08 17:54:34 -0300

    use selected colummns as uncertainty

--- R_functions_4_latex.R
+++ R_functions_4_latex.R
@@ -11,7 +11,13 @@
 # R packages used: stringr
 write_latexTable <- function(x,file,caption="",pos="htbp",align="c",label="tab:",
                              centering = TRUE, horizontal_lines = FALSE,
-                             siunitx_package = FALSE, booktabs_package = FALSE){
+                             siunitx_package = FALSE, booktabs_package = FALSE,
+                             cols_num=NULL,cols_error=NULL,error_digits=2){
+  # checks
+  if(length(cols_num) != length(cols_error))
+    stop("cols_error and cols_num should have same length")
+  if(length(cols_error) != 0 & siunitx_package == FALSE)
+    warning("cols_error and cols_num work together with siunitx_package set to TRUE")
   # inner functions
   make_latexTable_line <- function(raw_line){
     final_line <- paste(raw_line,collapse = " & ")
@@ -74,25 +80,51 @@ write_latexTable <- function(x,file,caption="",pos="htbp",align="c",label="tab:"
     writeLines(tblr_string_end,con)
   }
   wrt_tabular <- function(con,x,align,horizontal_lines,
-                          siunitx_package,booktabs_package){
+                          siunitx_package,booktabs_package,
+                          cols_num,cols_error,error_digits){
     # coersing a possible tibble to a dataframe
     x <- data.frame(x)
     
     # extracting basic info
     n <- nrow(x)
     n_col <- ncol(x)
-    header <- names(x)
+    n_errors <- length(cols_error)
+    cols_save <- (1:n_col)[!(1:n_col %in% cols_error)] # don't save error columns
+    header <- names(x)[cols_save]
     
     # start tabular environment
-    wrt_tblr_env_start(con,n_col,align)
+    wrt_tblr_env_start(con,n_col-n_errors,align)
     
     # add commands from siunitx package
     if(siunitx_package)
-      for(j in 1:ncol(x))
-        if(class(x[,j]) == "numeric")
-          # \num{} is a command from siunitx package which 
+      for(j in cols_save)
+        if(class(x[,j]) == "numeric"){
+          if(j %in% cols_num){
+            k <- cols_error[j == cols_num]
+            error_text <- paste("\\pm",format(as.list(x[,k]),scientific=F,
+                                              digits=error_digits),sep="")
+            # number of zeros between coma and principal number digits
+            nzeros <- ceiling(log10(1/abs(x[,j])))-1
+            nzeros <- ifelse(nzeros < 0,0,nzeros)
+            # number of decimal digits in principal number
+            nsmall <- ceiling(log10(1/x[,k])+error_digits-1)
+            nsmall <- ifelse(nsmall < 0,0,nsmall)
+            # number of total digits in principal number
+            digits <- floor(log10(abs(x[,j]))) + 1
+            digits <- ifelse(digits<0,0,digits) + nsmall - nzeros
+          } else{
+            error_text <- ""
+            nsmall = 0L
+            digits = NA
+          }
+          # \num{} is a command from siunitx package which
           # makes numbers format more pleasant
-          x[,j] <- paste("\\num{",x[,j],"}",sep="")
+          x_txt <- vector("character",n)
+          for(i in 1:n)
+            x_txt[i] <- format(as.list(x[i,j]),scientific=F,
+                               nsmall=nsmall[i],digits=digits[i])
+          x[,j] <- paste("\\num{",x_txt,error_text,"}",sep="")
+        }
     
     if(booktabs_package){
       txt_top_line <- "\t\t\t\\toprule"
@@ -106,12 +138,12 @@ write_latexTable <- function(x,file,caption="",pos="htbp",align="c",label="tab:"
     # write header and lines
     header_save <- make_latexTable_line(header)
     if(horizontal_lines)
-      writeLines(txt_top_line,con) 
+      writeLines(txt_top_line,con)
     writeLines(header_save,con)
     if(horizontal_lines)
       writeLines(txt_mid_line,con)
     for(i in 1:n){
-      line <- x[i,]
+      line <- x[i,cols_save]
       line_save <- make_latexTable_line(line)
       writeLines(line_save,con)
     }
@@ -128,7 +160,8 @@ write_latexTable <- function(x,file,caption="",pos="htbp",align="c",label="tab:"
   wrt_tbl_env_start(con,pos,caption,centering)
   # write tabular
   wrt_tabular(con,x,align,horizontal_lines,
-              siunitx_package,booktabs_package)
+              siunitx_package,booktabs_package,
+              cols_num,cols_error,error_digits)
   # end table environment
   wrt_tbl_env_end(con,label)
   # close file
